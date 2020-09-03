fabb248fec276460ce139b4223e47849cc39e410
Muresan Ramona <ramo.amo.ook@gmail.com>
2020-09-03 20:57:28 +0300

    asdf

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1. Hello \n" +
+                "Ramona");
+
+        System.out.println("2. Rezultatul este" + " " + (2 + 8));
+        System.out.println("3. Rezultatul este" + " " + (22 / 7f));
+        System.out.println("4. Rezultatele sunt: \n" +
+                "a." + " " + (-5 +8 *6) + "\n" + "b." + " " + ((55 + 9) % 9) +"\n" + "c." + " " + (20 + -3*5 / 8) + "\n"
+                + "d " + " " + (5 + 15 / 3 * 2 - 8 % 3));
+
+
+
+        int a , b , c, ageOfIulian;
+        a = 15;
+        b = 5;
+        c = 10;
+        ageOfIulian = a + b + c;
+        int dayOfMonth = 3;
+        int monthOfYear = 9;
+        int year = 2020;
+
+        System.out.println("Adunarea este egala cu:" + " " + (a + b + b ));
+        System.out.println( "Varsta Alinei = " + ageOfIulian + " " + "ani");
+        System.out.println( "Ioana este nascuta in anul "+ " " + ( year - monthOfYear + dayOfMonth ));
+        
+
+
+
+
+
+
+
+
+
+
+
+                // write your code here
+    }
+}
